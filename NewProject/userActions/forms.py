83ddc212fbef4d9218830043ed59306b4b59964d
KopninMaxim <79462434+KopninMaxim@users.noreply.github.com>
2021-03-19 16:15:22 +0700

    Убрал предлагаемые логины

--- NewProject/userActions/forms.py
+++ NewProject/userActions/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class UserRegisterForm(UserCreationForm):
     username = forms.CharField(label='Имя пользователя',
-                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
     password1 = forms.CharField(label='Пароль',
                                 widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Подтверждение пароля',
