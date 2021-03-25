dfd68395b2d68041190206a8583e32cd62dfa8b7
Andrew Seleznyov <andrselezen@mail.ru>
2021-03-26 02:58:47 +0700

    Исправил баг с фоткой

--- test_site/test_site/urls.py
+++ test_site/test_site/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from site_pages import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('about/', views.about_us),
     path('profile/', views.user_profile)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
